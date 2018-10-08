d91236cc32f71d359c7a394ebc726c42c326935a
Otyne <43828915+otyne@users.noreply.github.com>
2018-10-08 19:57:35 +0200

    no more > 13

--- my_compute_factorial_it.c
+++ my_compute_factorial_it.c
@@ -12,13 +12,13 @@ int my_compute_factorial_it(int nb)
     int i = 1;
     int fact = 1;
     
-    if (nb < 0)
+    if (nb < 0 ||nb > 12)
         return (0);
     if (nb == 0)
         return (1);
     while (i != nb + 1) {
         fact *= i;
-        i+=1;
+        i += 1;
     }
     return (fact);
 }

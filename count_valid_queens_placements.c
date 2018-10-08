11111cc071a5288190ce8d54dd35ec55a433ae8c
Otyne <43828915+otyne@users.noreply.github.com>
2018-10-08 02:33:21 +0200

    Add the last task "the n queens"
    recursion ftw

--- count_valid_queens_placements.c
+++ count_valid_queens_placements.c
@@ -7,7 +7,71 @@
 
 #include "my.h"
 
+static int board_different(int *chess, int nb)
+{
+    int i = 0;
+    int k = i + 1;
+
+    while (i != nb - 1) {
+        if (chess[k] == chess[i])
+            return (0);
+        k += 1;
+        if (k == nb) {
+            i += 1;
+            k = i + 1;
+        }
+    }
+    return (1);
+}
+
+static int left_to_right(int *chess, int nb)
+{
+    for (int i = 0; i != nb; i++) {
+        for (int k = i + 1; k != nb; k++) {
+            if (i + chess[k] == k + chess[i])
+                return (0);
+        }
+    }
+    return (1);
+}
+
+static int right_to_left(int *chess, int nb)
+{
+    for (int i = 0; i != nb; i++) {
+        for (int k = i + 1; k != nb; k++) {
+            if (i - chess[k] == k - chess[i])
+                return (0);
+        }
+    }
+    return (1);
+}
+
+static int create_possibilities(int *chess, int nb, int rec, int poss)
+{
+    int i = 0;
+
+    if (rec != nb + 1) {
+        for (i = 0; i != nb; i ++) {
+            chess[rec] = i;
+            poss = create_possibilities(chess, nb, rec + 1, poss);
+        }
+    }
+    if (rec == nb) {
+        if (board_different(chess, nb) == 1 &&
+            left_to_right(chess, nb) == 1 &&
+            right_to_left(chess, nb) == 1) {
+            poss += 1;
+        }
+    }
+    return (poss);
+}
+
 int count_valid_queens_placements(int nb)
 {
+    int poss = 0;
+    int rec = 0;
+    int chess[nb];
 
+    poss = create_possibilities(chess, nb, rec, poss);
+    return (poss);
 }
--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 
 #include "my.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int ac, char **av)
 {
-
+//    printf("%d\n", my_compute_square_root(atoi(av[1])));
+    printf("%d\n", count_valid_queens_placements(atoi(av[1])));
 }
